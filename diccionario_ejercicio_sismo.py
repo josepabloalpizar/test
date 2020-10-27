e009212fcf858d73ae8a3e6974b761a049ca1aa7
josepabloalpizar <35497940+josepabloalpizar@users.noreply.github.com>
2020-10-27 13:54:59 -0600

    Update diccionario_ejercicio_sismo.py
    update

--- diccionario_ejercicio_sismo.py
+++ diccionario_ejercicio_sismo.py
@@ -1,5 +1,5 @@
-class Lista():
-    def crearList(self):
+class Lista1():
+    def crearList1(self):
         self.lista=[]
         self.dic={}
 
@@ -15,9 +15,9 @@ class Lista():
             else:
                 self.dic[year]=[float(magnitud)]
         #print(self.dic)
-        generar.resultado()
+        generar1.resultado1()
 
-    def resultado(self):
+    def resultado1(self):
 
         for i in self.dic:
             cantidad=(len(self.dic.get(i)))
@@ -25,6 +25,67 @@ class Lista():
             print("año: " + i + "  Magnitud: " , self.dic.get(i) , "cantidad de sismos por año: ", cantidad , " promedio por año  " ,"%.2f"% promedio)
 
         
+print(" primer paso")
+generar1=Lista1()
+generar1.crearList1()
 
+
+
+class Lista():
+    def crearList(self):
+
+        self.dic={}
+        self.dic2={}
+
+        temblores=open("test.txt","r")
+        for linea in temblores:
+            linea1=linea.replace(",",".")
+            linea2=linea1.replace(";",",").strip()
+            year=linea2.split(',')[0] # 2015
+            month=linea2.split(',')[1] # 1
+            magnitud=linea2.split(',')[2] # 4.0b
+
+            if year in self.dic:
+                self.dic[year].append(str(month))
+            else:
+                self.dic[year]=[str(month)]
+
+            if year in self.dic:
+                self.dic[year].append(float(magnitud))
+            else:
+                self.dic[year]=[float(magnitud)]
+
+
+        for key, value in self.dic.items():
+
+            for element in value:
+
+
+                    mes=(self.dic2.get(key))
+                    anio="año: " + key
+
+     
+        for key, value in self.dic.items():
+
+            for element in value:
+
+                if type(element) is str:
+                    print("añ0",key,"mes",element)
+
+
+                if type(element) is float:
+                    print("magnitud del mes: ", element)
+
+                    print("")
+
+
+
+
+
+
+        
+
+print("")
+print(" segundo paso")
 generar=Lista()
 generar.crearList()
